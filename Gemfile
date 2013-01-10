444753cefc856084dbe4b7f20f10a7790f3d7662
Jared Beck <jared@jaredbeck.com>
2013-01-10 00:36:00 -0500

    drop dependency on ruby, use heroku toolbelt, various other improvements

--- Gemfile
+++
@@ -1,5 +0,0 @@
-source :rubygems
-
-gem 'heroku'
-# gem 'sqlite3'
-# gem 'taps'
--- backup.sh
+++ backup.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
+set -e
 
 # Note: Script documentation has been moved to README.md
 echo "Starting backup of gocongress data" | logger
 
 # Configuration
-backupdir="/Users/jared/git/usgo/gocongress/backup"
+heroku_bin='/usr/bin/heroku'
+backup_root="/Users/jared/git/usgo/gocongress/backup"
+month_stamp=`date "+%Y-%m"`
 curdate=`date "+%Y-%m-%d-%H-%M"`
 
-# Load RVM because we don't want to use the system ruby ..
-if [[ -s ~/.rvm/scripts/rvm ]]; then
-  . ~/.rvm/scripts/rvm
-else
-  echo "Fatal: Cannot find RVM" | logger
+# Make sure Heroku toolbelt is installed
+if [ ! -x $heroku_bin ]; then
+  echo "Fatal: File not executable: $heroku_bin" | logger
   exit 1
 fi
 
-# .. we want to use the project's ruby (and gemset).
-# Therefore, cd into the backup dir causing its .rvmrc to run
-cd $backupdir
-echo "Heroku gem version is: $(heroku version)" | logger
+echo "Heroku version is: $(heroku version)" | logger
 
-# Old method: Use taps to pull into a sqlite db
-# heroku db:pull sqlite://$backupdir/$curdate.db --app gocongress --confirm gocongress
+# Make the monthly dir, if needed
+backup_dir="$backup_root/$month_stamp"
+if [ ! -d $backup_dir ]; then
+  mkdir $backup_dir
+fi
 
-# New method: Use pgbackups to pull into a psql dump
-# capture --expire automatically deletes oldest backup
+# Use pgbackups to pull into a psql dump
+# `capture --expire` automatically deletes oldest backup
 echo "Starting pgbackups capture" | logger
 heroku pgbackups:capture --expire --app gocongress | logger
 bkp_url=`heroku pgbackups:url --app gocongress`
-curl -o $backupdir/$curdate.dump $bkp_url
-du -h $backupdir/$curdate.dump | logger
+dump_path="$backup_dir/$curdate.dump"
+curl -o $dump_path $bkp_url
+du -h $dump_path | logger
 
 # stick a fork in it, we are done
 echo "Done with backup of gocongress data" | logger
